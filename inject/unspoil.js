08db48483125ef1336ce67b873f5a8a8441233a5
Kyle Coburn <kylecoburn@gmail.com>
2017-09-23 14:07:56 -0700

    Workaround seek slider clicking

--- inject/unspoil.js
+++ inject/unspoil.js
@@ -5,42 +5,56 @@ const SEEK_SECONDS = [
   60 * 30,
 ]
 
-const seek = function (event) {
+const onSeek = function (event) {
   const seekVector = parseInt(event.currentTarget.getAttribute('u-seek'))
   if (isNaN(seekVector)) {
     console.error('Invalid seek target', event.currentTarget, event.target)
     return
   }
-
-  const slider = document.querySelector('.js-player-slider')
-  const sliderRect = slider.getBoundingClientRect()
-  const maxSliderPixels = sliderRect.width
-
-  const seekSecondsNow = parseFloat(slider.getAttribute('aria-valuenow'))
-  const seekSecondsMax = parseFloat(slider.getAttribute('aria-valuemax'))
   const seekIndex = Math.abs(seekVector) - 1
-  const resultHandleSeconds = seekSecondsNow + (Math.sign(seekVector) * SEEK_SECONDS[seekIndex])
-  let resultHandlePixels
-  if (resultHandleSeconds < 0) {
-    if (seekSecondsNow < 1) {
-      return
-    }
-    resultHandlePixels = 0
-  } else if (resultHandleSeconds > seekSecondsMax) {
-    if (seekSecondsNow > seekSecondsMax - 1) {
-      return
-    }
-    resultHandlePixels = maxSliderPixels
-  } else {
-    resultHandlePixels = (resultHandleSeconds / seekSecondsMax) * maxSliderPixels
-  }
-  const seekMouseEvent = new MouseEvent('click', {
-    bubbles: true,
-    cancelable: true,
-    clientX: sliderRect.left + resultHandlePixels,
-    clientY: sliderRect.top,
-  })
-  slider.dispatchEvent(seekMouseEvent)
+  const seekDuration = SEEK_SECONDS[seekIndex]
+
+  // const slider = document.querySelector('.js-player-slider')
+  // const sliderRect = slider.getBoundingClientRect()
+  // const maxSliderPixels = sliderRect.width
+  // const seekSecondsNow = parseFloat(slider.getAttribute('aria-valuenow'))
+  // const seekSecondsMax = parseFloat(slider.getAttribute('aria-valuemax'))
+  // const resultHandleSeconds = seekSecondsNow + (Math.sign(seekVector) * seekDuration)
+  // let resultHandlePixels
+  // if (resultHandleSeconds < 0) {
+  //   if (seekSecondsNow < 1) {
+  //     return
+  //   }
+  //   resultHandlePixels = 0
+  // } else if (resultHandleSeconds > seekSecondsMax) {
+  //   if (seekSecondsNow > seekSecondsMax - 1) {
+  //     return
+  //   }
+  //   resultHandlePixels = maxSliderPixels
+  // } else {
+  //   resultHandlePixels = (resultHandleSeconds / seekSecondsMax) * maxSliderPixels
+  // }
+  // const seekMouseEvent = new MouseEvent('click', {
+  //   bubbles: true,
+  //   cancelable: true,
+  //   clientX: sliderRect.left + resultHandlePixels,
+  //   clientY: sliderRect.top,
+  // })
+  // slider.dispatchEvent(seekMouseEvent)
+  // slider.setAttribute('aria-valuenow', resultHandleSeconds)
+  // console.log(event, seekMouseEvent)
+
+   const playerElement = document.querySelector('#player')
+   const keyEvent = new KeyboardEvent('keydown', {
+     bubbles: false,
+     cancelable: false,
+     key: seekVector < 0 ? 'ArrowLeft' : 'ArrowRight',
+     repeat: true,
+   })
+   const arrowSeeks = Math.ceil(seekDuration / 5)
+   for (let idx = 0; idx < arrowSeeks; idx += 1) {
+     playerElement.dispatchEvent(keyEvent)
+   }
 }
 
 const pageObserver = new window.MutationObserver((mutations, observing) => {
@@ -73,7 +87,7 @@ const pageObserver = new window.MutationObserver((mutations, observing) => {
     `
     for (const spanChild of unspoilDiv.children) {
       for (const buttonChild of spanChild.children) {
-        buttonChild.addEventListener('click', seek, false)
+        buttonChild.addEventListener('click', onSeek, false)
       }
     }
   }
