5684d64ee2bc189451fa5a7b9326e0c60afbe68c
Kyle Coburn <kylecoburn@gmail.com>
2017-10-19 11:47:34 -0700

    Fix error checking callback

--- inject/util.js
+++ inject/util.js
@@ -3,14 +3,14 @@ const waitForSelector = function (selector, callback, maxAttemptFrames) {
   const waitInterval = window.setInterval(() => {
     attempts += 1
     const element = document.querySelector(selector)
-    if (element) {
-      callback(element)
-    }
     if (element || attempts > maxAttemptFrames) {
       if (attempts > 999) {
         console.error('Failed to load', selector, attempts)
       }
       window.clearInterval(waitInterval)
+    }
+    if (element) {
+      callback(element)
     }
   }, 0)
 }
